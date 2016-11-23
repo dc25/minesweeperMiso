e92c6e33acdd796791915e599fa02df908aa2f40
Dave Compton <davecompton7@gmail.com>
2016-11-23 21:29:06 +0000

    not recursing on mined cell even if count is zero

--- Main.hs
+++ Main.hs
@@ -2,7 +2,6 @@
 {-# LANGUAGE OverloadedStrings #-}
 import Reflex
 import Reflex.Dom
-import Debug.Trace
 import Control.Monad.Random (RandomGen, Rand, runRand, getStdGen, getRandomR)
 import Control.Monad.Trans (liftIO)
 import Control.Monad.State (State, state, runState)
@@ -41,12 +40,7 @@ mkBoard = do
     return $ fromList $ zip positions cells 
 
 getColor :: Cell -> String
-getColor (Cell mined exposed flagged) = 
-    case (mined,   exposed, flagged) of
-         (      _,       _,    True) -> "#AAAAAA"
-         (      _,   False,       _) -> "#AAAAAA"
-         (  True ,       _,       _) -> "#AAAAAA"
-         (  False,       _,       _) -> "#909090"
+getColor (Cell _ exposed _) = if exposed then "#909090" else "#AAAAAA"
 
 cellAttrs :: Cell -> Map Text Text
 cellAttrs cell = 
@@ -182,8 +176,8 @@ showCell board pos c@(Cell mined exposed flagged) =
     in case (  mined, exposed, flagged, count) of
             (      _,       _,    True,     _) -> showWithFlag    board pos c
             (      _,   False,       _,     _) -> showWithoutText board pos c
-            (      _,    True,       _,     0) -> showWithoutText board pos c
             (   True,    True,       _,     _) -> showWithMine    board pos c
+            (      _,    True,       _,     0) -> showWithoutText board pos c
             (      _,       _,       _,     _) -> showWithText    board pos c
 
 adjacents :: Pos -> [Pos]
@@ -206,17 +200,16 @@ fromLeftPickM (x,y) =
                 count = length $ filter mined $ fmap (board !) indices
                 c = board ! (x,y)
                 
-                updatedCell = if (not $ flagged c) -- can't expose a flagged cell.
-                              then c {exposed=True} 
-                              else c
+                updatedCell = if (flagged c) -- can't expose a flagged cell.
+                              then c
+                              else c {exposed=True} 
 
                 updatedBoard = insert (x,y) updatedCell board 
 
-                checkList = (if     not (exposed c) 
-                                 && not (flagged c) 
-                                 && count == 0 
-                             then indices 
-                             else [] )
+                checkList = (if (exposed c) || (flagged c) || (mined c) || count /= 0 
+                             then [] 
+                             else indices 
+                             )
 
                 neighborUpdater = mapM fromLeftPickM checkList
                 (updatedNeighbors, updatedNeighborsBoard) = runState neighborUpdater updatedBoard
@@ -228,7 +221,7 @@ fromPick board (LeftPick p c) =
     in nc
 
 fromPick board (RightPick pos c) = 
-    if (exposed $ trace ("Right pick on " ++ show c) c)
+    if exposed c
     then [] -- can't flag a cell that's already exposed.
     else [(pos, Just c {flagged=not $ flagged c})]
 
@@ -244,7 +237,7 @@ boardAttrs = fromList
                  ]
 
 
-showBoard :: MonadWidget t m => m ()
+showBoard :: MonadWidget t m => m Int
 showBoard = do
     gen <- liftIO getStdGen
     let (initialBoard, _)  = runRand mkBoard gen
@@ -257,10 +250,13 @@ showBoard = do
             eventMap = fmap (fmap (fmap fst)) eventAndCellMap
         cm <- cellMap 
         (_, ev) <- elSvgns "svg" (constDyn boardAttrs) $ eventMap
-    return ()
+    return 5
 
 main :: IO ()
-main = mainWidget showBoard
+main = mainWidget $ do 
+                       v <- showBoard; 
+                       text $ pack $ show v
+                       return ()
 
 -- At end to avoid Rosetta Code unmatched quotes problem.
 elSvgns :: MonadWidget t m => Text -> Dynamic t (Map Text Text) -> m a -> m (El t, a)
